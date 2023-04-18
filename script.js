cab1822e4582fe11b4b9df6c78dc2e31854a8daa
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-04-17 21:58:30 -0400

    Fixed js files and added pointer to cursor

--- script.js
+++ script.js
@@ -1,101 +1,122 @@
 window.addEventListener('load', () => {
-  
-    const tab_switchers = document.querySelectorAll('[data-switcher]');
 
-    for (let i = 0; i < tab_switchers.length; i++) {
-        const tab_switcher = tab_switchers[i];
-        const page_id = tab_switcher.dataset.tab;
+  const myIcon = document.getElementById("my-icon");
 
-        tab_switcher.addEventListener('click', () => {
-            document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
-            tab_switcher.parentNode.classList.add('is-active'); 
+  myIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
 
-            SwitchPage(page_id);
-        });
-    }
-    function SwitchPage (page_id) {
-      console.log(page_id);
-    
-      const current_page = document.querySelector('.pages .page.is-active');
-      current_page.classList.remove('is-active');
-    
-      const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
-      next_page.classList.add('is-active');
-    }
+  <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
 
-    const form = document.querySelector("#submission-form");
-    const experience_text = document.querySelector("#experience-text-input");
-    var submittions_li = document.querySelector("#home-submittions");
-  
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-  
-      const submittion = experience_text.value;
-      if(!submittion){
-        alert("Please write something!");
-        return;
-      }
-      const boroughs = document.querySelector("#boroughs-dropdown");
-      
+  </svg>`;
 
-      if (boroughs.value == "Manhattan") {
-        submittions_li = document.querySelector("#manhattan-submittions");
-        AddElement(submittions_li);
-      }
-      if (boroughs.value == "Brooklyn") {
-        submittions_li = document.querySelector("#brooklyn-submittions");
-        AddElement(submittions_li);
-      }
-      if (boroughs.value == "Queens") {
-        submittions_li = document.querySelector("#queens-submittions");
-        AddElement(submittions_li);
-      }
-      if (boroughs.value == "The Bronx") {
-        submittions_li = document.querySelector("#bronx-submittions");
-        AddElement(submittions_li);
-      }
-      if (boroughs.value == "Staten Island") {
-        submittions_li = document.querySelector("#staten-island-submittions");
-        AddElement(submittions_li);
-      }
-      
-      function AddElement (list) {
-        const submittion_el = document.createElement("div");
-        submittion_el.classList.add('container-sm', 'justify-content-center', 'bg-black', 'pt-5', 'pb-5', 'mb-5', 'rounded-pill');
-        
-        const submittion_content = document.createElement("div");
-        submittion_content.classList.add('text-white', 'text-center');
-        const upvote_btn_el = document.createElement("div");
-        upvote_btn_el.classList.add("actions");
-    
-        submittion_el.appendChild(submittion_content);
-        submittion_el.appendChild(upvote_btn_el);
-    
-        const submittion_input = document.createElement("input");
-        submittion_input.classList.add("text");
-        submittion_input.type = "text";
-        submittion_input.value = submittion;
-        submittion_input.setAttribute("readonly", "readonly");
-  
+const tab_switchers = document.querySelectorAll('[data-switcher]');
 
-        const btn_obj = document.createElement("button");
-        btn_obj.classList.add("man-btn");
-        btn_obj.classList.add("bi");
-        btn_obj.setAttribute('border-2', 'ms-5', 'btn-outline-light', "upvote")
-        upvote_btn_el.appendChild(btn_obj);
-        BtnPress();
+for (let i = 0; i < tab_switchers.length; i++) {
+  const tab_switcher = tab_switchers[i];
+  const page_id = tab_switcher.dataset.tab;
 
-    
-        submittion_content.appendChild(submittion_input);
-        list.appendChild(submittion_el);
-    
-      
-        alert("Submitted!");
-        return;
-      }
-      function BtnPress (btn) {
-        
-      }
- 
-    }) 
+  tab_switcher.addEventListener('click', () => {
+      document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
+      tab_switcher.parentNode.classList.add('is-active');
+
+      SwitchPage(page_id);
+  });
+}
+function SwitchPage (page_id) {
+console.log(page_id);
+
+const current_page = document.querySelector('.pages .page.is-active');
+current_page.classList.remove('is-active');
+
+const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+next_page.classList.add('is-active');
+}
+
+const form = document.querySelector("#submission-form");
+const experience_text = document.querySelector("#experience-text-input");
+var submittions_li = document.querySelector("#home-submittions");
+
+form.addEventListener('submit', (e) => {
+e.preventDefault();
+
+const submittion = experience_text.value;
+if(!submittion){
+  alert("Please write something!");
+  return;
+}
+const boroughs = document.querySelector("#boroughs-dropdown");
+
+
+if (boroughs.value == "Manhattan") {
+  submittions_li = document.querySelector("#manhattan-submittions");
+  AddElement(submittions_li);
+}
+if (boroughs.value == "Brooklyn") {
+  submittions_li = document.querySelector("#brooklyn-submittions");
+  AddElement(submittions_li);
+}
+if (boroughs.value == "Queens") {
+  submittions_li = document.querySelector("#queens-submittions");
+  AddElement(submittions_li);
+}
+if (boroughs.value == "The Bronx") {
+  submittions_li = document.querySelector("#bronx-submittions");
+  AddElement(submittions_li);
+}
+if (boroughs.value == "Staten Island") {
+  submittions_li = document.querySelector("#staten-island-submittions");
+  AddElement(submittions_li);
+}
+
+function AddElement (list) {
+
+  //Submission Element
+  const submittion_el = document.createElement("div");
+  submittion_el.classList.add('container-sm', 'justify-content-center', 'bg-black', 'pt-5', 'pb-5', 'mb-5', 'rounded-pill');
+  //Content of Submission
+  const submittion_content = document.createElement("div");
+  submittion_content.classList.add("cont-div");
+
+
+  //Upvote button of submission
+  const upvote_btn = document.createElement("button");
+  const thumbs_up = document.createElement("span");
+
+  const myIcon = document.getElementById("my-icon");
+
+  myIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
+
+  <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"/>
+
+  </svg>`;
+
+  upvote_btn.classList.add('btn-like', 'btn', 'ms-5', 'btn-outline-light');
+  thumbs_up.appendChild(myIcon);
+
+  upvote_btn.appendChild(thumbs_up);
+  upvote_btn.textContent = 'up-vote';
+  var count = 0;
+
+  upvote_btn.addEventListener('click', () => {
+    count++;
+    console.log(experience_text.value + ": " + count);
   })
+  submittion_el.appendChild(submittion_content);
+  submittion_el.appendChild(upvote_btn);
+  
+  
+  const submittion_input = document.createElement("p");
+    submittion_input.classList.add('text-white', 'text-center', 'h5');
+    submittion_input.textContent = submittion;
+
+
+  //submittion_input.setAttribute("readonly", "readonly");
+
+  submittion_content.appendChild(submittion_input);
+  list.appendChild(submittion_el);
+
+
+  alert("Submitted!");
+  return;
+}
+})
+})
