f3cb4ffd65be82e28cca38f02c011ad5a2ddd863
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-05-11 16:20:20 -0400

    Updated navbar + form selection

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
 
-  const test = document.querySelector('.pages .box .bx-hover')
-
+  const test = document.querySelector('.pages .box .bx-hover');
+  behavior();
   const tab_switchers = document.querySelectorAll('[data-switcher]');
 
   for (let i = 0; i < tab_switchers.length; i++) {
@@ -37,9 +37,6 @@ window.addEventListener('load', () => {
     }); 
   })
   
-
-
-
   function SwitchPage (page_id) {
   console.log(page_id);
 
@@ -184,4 +181,57 @@ window.addEventListener('load', () => {
       return;
     }
   })
+  function behavior (){  
+    //svg color for manhattan
+    const svgManPath = document.getElementById('man-svg');
+
+    document.getElementById('man-btn').addEventListener('mouseover', () => {
+        svgManPath.classList.add('svg-btn-hover');
+    });
+
+    document.getElementById('man-btn').addEventListener('mouseout', () => {
+        svgManPath.classList.remove('svg-btn-hover');
+    });
+
+    //svg color for bronx
+    const svgBronxPath = document.getElementById('bronx-svg');
+
+    document.getElementById('bron-btn').addEventListener('mouseover', () => {
+        svgBronxPath.classList.add('svg-btn-hover');
+    });
+
+    document.getElementById('bron-btn').addEventListener('mouseout', () => {
+        svgBronxPath.classList.remove('svg-btn-hover');
+    });
+    //svg color for brooklyn
+    const svgBrookPath = document.getElementById('brook-svg');
+
+    document.getElementById('brook-btn').addEventListener('mouseover', () => {
+        svgBrookPath.classList.add('svg-btn-hover');
+    });
+
+    document.getElementById('brook-btn').addEventListener('mouseout', () => {
+        svgBrookPath.classList.remove('svg-btn-hover');
+    });
+    //svg color for queens
+    const svgQueensPath = document.getElementById('queens-svg');
+
+    document.getElementById('queens-btn').addEventListener('mouseover', () => {
+        svgQueensPath.classList.add('svg-btn-hover');
+    });
+
+    document.getElementById('queens-btn').addEventListener('mouseout', () => {
+        svgQueensPath.classList.remove('svg-btn-hover');
+    });
+    //svg color for staten island
+    const svgStatenPath = document.getElementById('staten-svg');
+
+    document.getElementById('staten-btn').addEventListener('mouseover', () => {
+        svgStatenPath.classList.add('svg-btn-hover');
+    });
+
+    document.getElementById('staten-btn').addEventListener('mouseout', () => {
+        svgStatenPath.classList.remove('svg-btn-hover');
+    });
+  }
 })
